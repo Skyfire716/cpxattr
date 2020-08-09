4f750b5d864e771b61c8ef7209ca430ebf72a91f
Skyfire716 <skyfire716@gmail.com>
2020-08-09 15:26:14 +0200

    Update cpxattr.sh
    Copies all Attributes

--- cpxattr.sh
+++ cpxattr.sh
@@ -3,6 +3,10 @@ current="$PWD"
 src="$current"/"$1"
 dst="$current"/"$2"
 Attr="$(getfattr --absolute-names  -d "$src" | grep -v '# file')"
-tags="$(echo $Attr | cut -d'=' -f1)"
-vals="$(echo $Attr | cut -d'=' -f2)"
-setfattr -n $tags -v $vals "$dst"
+lines="$(echo "$Attr" | wc -l)"
+for (( i=1; i <=$lines; i++ ))
+do
+    tags="$(echo "$Attr" | cut -d'=' -f1 | sed $i'q;d')"
+    val="$(echo "$Attr" | cut -d'=' -f2 | sed $i'q;d')"
+    setfattr -n $tags -v $val "$dst"
+done
